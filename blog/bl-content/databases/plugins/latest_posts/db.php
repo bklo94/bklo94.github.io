ed8efd9a6b34461b0d351503812d99ba6d6b5d6c
Brandon Lo <brandonklo94@gmail.com>
2018-04-30 01:01:12 -0700

    Rebuilt Website
    Rebuilt wesite to make it more simple and less resource intensive

--- blog/bl-content/databases/plugins/latest_posts/db.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{
-    "label": "Latest posts",
-    "amount": 5,
-    "position": 0
-}
--- blog/bl-kernel/boot/rules/99.header.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.');
-
-// Page not found 404
-if($Url->notFound())
-{
-	header('HTTP/1.0 404 Not Found');
-}
